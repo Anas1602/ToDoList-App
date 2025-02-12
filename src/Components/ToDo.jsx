690544d12761dcddd8aa2037e0be278bad67cc1d
Anas LARIBI <anas.laribi95@gmail.com>
2025-02-12 04:47:09 -0500

    Starting a ToDo List App

---
+++ src/Components/ToDo.jsx
@@ -0,0 +1,7 @@
+const ToDo = () => {
+  return (
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl"></div>
+  );
+};
+
+export default ToDo;
